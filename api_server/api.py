4af197ba6b9c46e12d84bc52e5902316af405f01
kevin93203 <kevin93203@gmail.com>
2023-06-21 18:19:11 +0800

    fastapi add two api: [members/detail] and [snstimeslines]

--- api_server/api.py
+++ api_server/api.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
 import requests
+from bs4 import BeautifulSoup
 import uvicorn
 from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -20,13 +22,68 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-@app.get("/api/member/list/")
-async def root():
+@app.get("/api/member/list")
+async def getMemberList():
     url = "https://www.akb48.co.jp/public/api/member/list/"
     headers = {
         "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"
     }
-    response = requests.post(url,headers=headers)
+    response = requests.post(url,headers=headers,verify=False)
+    return response.json()
+
+@app.get("/api/members/detail")
+async def getMemberDetail(
+    mid:int
+):
+    url = f"https://www.akb48.co.jp/about/members/detail?mid={mid}"
+    headers = {
+        "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"
+    }
+    response = requests.get(url, headers=headers, verify=False)
+    memberDetail = {}
+
+    soup = BeautifulSoup(response.text, 'lxml')
+    memberDetail['teamIcon'] = 'https://www.akb48.co.jp' + soup.select_one('.teamIcon').find("img")['src']
+    memberDetail['teamMedal'] = 'https://www.akb48.co.jp' + soup.select_one('.teamMedal').find("img")['src']
+    memberDetail['memberImg'] = soup.select_one('.memberImg').find("img")['src']
+    memberDetail['jpname'] = soup.select_one('.jpname').text
+    memberDetail['name'] = soup.select_one('.name').text
+    memberDetail['enName'] = soup.select_one('.enName').text
+
+    detailProfTxt = soup.select_one('.detailProfTxt').find_all('dd')
+    deatailProfs = ["agency","nickname","birthday","birthPlace"]
+
+    for i,detailProf in enumerate(detailProfTxt):
+        if deatailProfs[i] == 'birthday':
+            birthday = f'{detailProf.text[0:4]}-{detailProf.text[5:7]}-{detailProf.text[8:10]}'
+            memberDetail[deatailProfs[i]] = birthday 
+        else:    
+            memberDetail[deatailProfs[i]] = detailProf.text
+    
+    shareBlk = soup.select_one('.shareBlk')
+    memberDetail['twitter'] = shareBlk.select_one('.svgTw').find('a')['href']
+    memberDetail['instagram'] = shareBlk.select_one('.svgIg').find('a')['href']
+    
+    return {"result": "ok", "data": memberDetail}
+
+class snsQuerySetting(BaseModel):
+    start: int
+    limit: int
+    members: int
+
+@app.post("/api/snstimelines")
+async def getMemberSNSTimeLines(
+    snsQuerySetting:snsQuerySetting
+):
+    url = "https://www.akb48.co.jp/public/api/snstimelines/"
+
+    headers = {
+        "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"
+    }
+
+    data = snsQuerySetting.dict()
+
+    response = requests.post(url,headers=headers,data=data,verify=False)
     return response.json()
 
 if __name__ == "__main__":
